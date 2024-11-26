0f8336e2cb609cf3295148089b0bf5c026b06136
Lucas Class <lucasclass94@gmail.com>
2024-11-26 13:31:49 -0300

    feat: add botao styles, add large logo, add page functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Page from "@/components/_template/page";
+import LogoLarge from "@/components/_template/logoLarge";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Page>
-      <h1>Hello World!</h1>
-    </Page>
+    <main className="h-screen flex flex-col justify-center items-center bg-[url('/background-elementos.svg')] bg-cover gap-10">
+      <div className="flex flex-col items-center gap-4">
+        <LogoLarge />
+        <p className="text-zinc-500 font-light w-96 leading-6">
+          Crie e gerencie o convite do seu evento de forma rápida e fácil, sem
+          complicações!
+        </p>
+      </div>
+      <Link href="/evento" className="botao azul text-lg">
+        Crie o seu Evento
+      </Link>
+    </main>
   );
 }
